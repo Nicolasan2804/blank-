7f7c140e08cbb4befabafdd52adb6b2e99c3fe41
Nicolas <an@et.intechinfo.fr>
2023-03-27 14:08:31 +0200

    modifv1

--- projet.js
+++ projet.js
@@ -1,11 +1,23 @@
-class Person{
-    constructor(nom, age=0){
-        this.nom=nom;
-        this.age=age;
-        this.ismajeur=(this.age>=18);
+class Person {
+    constructor(nom, age = 0) {
+        this.nom = nom;
+        this.age = age;
+        this.ismajeur = (this.age >= 18);
 
 
     }
 
-
+    salutation() {
+        if (this.ismajeur) {
+            console.log("Bonjour" + this.nom + ", tu as" + this.age + " ans donc tu es majeur.");
+        }
+        else {
+            console.log(`Bonjour ${this.nom}, tu as ${this.age} ans donc tu es mineur. `);
+        }
+    }
 }
+
+const person1 = new Person("Nicolas", 20);
+person1,salutation();
+const person2 = new Person("Sylvain",23);
+person2,salutation();
